370e3ac76840d978dddcd11edf7b09d313ccfa88
chrimaho <chrismahoney@hotmail.com>
2022-07-02 22:21:00 +1000

    Add `stability` tests

---
+++ ts_stat_tests/tests/test_stability.py
@@ -0,0 +1,13 @@
+from ts_stat_tests.stability import is_stable
+from ts_stat_tests.stability import stability
+from ts_stat_tests.tests import BaseTester
+
+
+class StabilityTests(BaseTester):
+    def setUp(self):
+        self.stability = stability(self.data)
+        self.is_stable = is_stable(self.data)
+
+    def test_stability(self):
+        self.assertEqual(self.stability, 12702.672087912088)
+        self.assertTrue(self.is_stable)
