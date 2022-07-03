dad4da048904caecd473b0826fd5801d329360a8
chrimaho <chrismahoney@hotmail.com>
2022-07-03 13:27:48 +1000

    Add `lumpiness` functions

--- src/stability.py
+++ src/stability.py
@@ -1,16 +1,26 @@
+from typing import Union
+
+import numpy as np
 import pandas as pd
+from tsfeatures import lumpiness as ts_lumpiness
 from tsfeatures import stability as ts_stability
 
 # from src.utils.docstrings import overwrite_docs_from
 
+
+# ------------------------------------------------------------------------------#
+# Stability                                                                 ####
+# ------------------------------------------------------------------------------#
+
+
 # overwrite_docs_from(parent_function=ts_stability)
-def stability(data: pd.Series, freq: int = 1) -> float:
+def stability(data: Union[np.array, pd.DataFrame, pd.Series], freq: int = 1) -> float:
     """
     !!! Summary
         Test for stability.
 
     Params:
-        data (pd.Series):
+        data (Union[np.array, pd.DataFrame, pd.Series]):
             The time series.
         freq (int, optional):
             Frequency of the time series
@@ -37,13 +47,15 @@ def stability(data: pd.Series, freq: int = 1) -> float:
     return ts_stability(x=data, freq=freq)["stability"]
 
 
-def is_stable(data: pd.Series, freq: int = 1, alpha: float = 0.5) -> bool:
+def is_stable(
+    data: Union[np.array, pd.DataFrame, pd.Series], freq: int = 1, alpha: float = 0.5
+) -> bool:
     """
     !!! Summary
         Check whether a data series is stable or not.
 
     Params:
-        data (pd.Series):
+        data (Union[np.array, pd.DataFrame, pd.Series]):
             The time series.
         freq (int, optional):
             The frequency of the time series. Defaults to `1`.
@@ -52,7 +64,7 @@ def is_stable(data: pd.Series, freq: int = 1, alpha: float = 0.5) -> bool:
 
     Returns:
         (bool):
-            A confirmaiont of whether or nto the data is stable.
+            A confirmaiont of whether or not the data is stable.
 
     ??? Note
         For full documentation, see:
@@ -70,3 +82,78 @@ def is_stable(data: pd.Series, freq: int = 1, alpha: float = 0.5) -> bool:
         ```
     """
     return True if stability(data=data, freq=freq) > alpha else False
+
+
+# ------------------------------------------------------------------------------#
+# Lumpiness                                                                 ####
+# ------------------------------------------------------------------------------#
+
+
+def lumpiness(data: Union[np.array, pd.DataFrame, pd.Series], freq: int = 1) -> float:
+    """
+    !!! Summary
+        Test for lumpiness.
+
+    Params:
+        data (Union[np.array, pd.DataFrame, pd.Series]):
+            The time series.
+        freq (int, optional):
+            Frequency of the time series
+
+    Returns:
+        (float):
+            Variance of the means of tiled windows.
+
+    ??? Note
+        For full documentation, see:
+
+        - Python: [`tsfeatures`](https://github.com/Nixtla/tsfeatures/blob/master/tsfeatures/tsfeatures.py)
+        - R: [`tsfeatures`](http://pkg.robjhyndman.com/tsfeatures/reference/lumpiness.html)
+
+    ??? Example "Examples"
+        Basic usage:
+        ```python linenums="1"
+        >>> from sktime.datasets import load_airline
+        >>> data = load_airline()
+        >>> print(lumpiness(data))
+        5558930.856730431
+        ```
+    """
+    return ts_lumpiness(x=data, freq=freq)["lumpiness"]
+
+
+def is_lumpy(
+    data: Union[np.array, pd.DataFrame, pd.Series], freq: int = 1, alpha: float = 0.5
+) -> bool:
+    """
+    !!! Summary
+        Check whether a data series is lumpy or not.
+
+    Params:
+        data (Union[np.array, pd.DataFrame, pd.Series]):
+            The time series.
+        freq (int, optional):
+            The frequency of the time series. Defaults to `1`.
+        alpha (float, optional):
+            The value, above which, the data will be stable. Defaults to `0.5`.
+
+    Returns:
+        (bool):
+            A confirmaiont of whether or not the data is lumpy.
+
+    ??? Note
+        For full documentation, see:
+
+        - Python: [`tsfeatures`](https://github.com/Nixtla/tsfeatures/blob/master/tsfeatures/tsfeatures.py)
+        - R: [`tsfeatures`](http://pkg.robjhyndman.com/tsfeatures/reference/lumpiness.html)
+
+    ??? Example "Examples"
+        Basic usage:
+        ```python linenums="1"
+        >>> from sktime.datasets import load_airline
+        >>> data = load_airline()
+        >>> print(is_lumpy(data))
+        True
+        ```
+    """
+    return True if lumpiness(data=data, freq=freq) > alpha else False
