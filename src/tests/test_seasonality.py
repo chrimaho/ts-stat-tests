4ed6b2a1d998ed4ccf828dee2a0f03c5c101146d
chmahone <chris.mahoney@dbschenker.com>
2022-07-06 14:42:51 +1000

    Add unittest for QS test

---
+++ src/tests/test_seasonality.py
@@ -0,0 +1,62 @@
+from src.tests.test_base import BaseTester
+from src.seasonality import qs
+from pmdarima.arima import ARIMA
+
+from src.utils.dict_helpers import dict_slice_by_keys
+
+class CorrelationTests(BaseTester):
+    
+    def setUp(self):
+        self.qs_result = qs(self.data, freq=12)
+
+    def test_qs(self):
+        
+        self.assertIsInstance(self.qs_result, dict)
+        self.assertListEqual(list(self.qs_result.keys()), ["stat", "Pval", "test", "model"])
+        self.assertIsInstance(self.qs_result['stat'], float)
+        self.assertIsInstance(self.qs_result['Pval'], float)
+        self.assertIsInstance(self.qs_result['test'], str)
+        self.assertIsInstance(self.qs_result['model'], (dict, type(None), ARIMA))
+        
+        # To get the full list of necessary permutations, run:
+        # print(list(itertools.product([[True,False]]*3)))
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, True, True), ['stat','Pval']),
+            {'stat': 101.85929391917927, 'Pval': 7.612641184541459e-23}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, True, False), ['stat','Pval']),
+            {'stat': 131.44999855937297, 'Pval': 2.85756086669558e-29}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, False, True), ['stat','Pval']),
+            {'stat': 194.4692892087745, 'Pval': 5.90922325801522e-43}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, False, False), ['stat','Pval']),
+            {'stat': 194.4692892087745, 'Pval': 5.90922325801522e-43}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, True, True), ['stat','Pval']),
+            {'stat': 132.58238294394926, 'Pval': 1.6221885995406117e-29}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, True, True), ['stat','Pval']),
+            {'stat': 145.12096239944535, 'Pval': 3.071732627217893e-32}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, True, True), ['stat','Pval']),
+            {'stat': 141.71278764765492, 'Pval': 1.6883370644788516e-31}
+        )
+
+        self.assertDictEqual(
+            dict_slice_by_keys(qs(self.data, 12, True, True, True), ['stat','Pval']),
+            {'stat': 141.71278764765492, 'Pval': 1.6883370644788516e-31}
+        )
